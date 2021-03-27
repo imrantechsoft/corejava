937fba1f14913284898514d03f1e38f937bd10e4
Imran <imranbashamg@gmail.com>
2021-03-27 12:16:19 +0530

    Added PalindromeNumber program to project

---
+++ corejava/src/main/java/com/imrantechsoft/corejava/PalindromeNumber.java
@@ -0,0 +1,25 @@
+package com.imrantechsoft.corejava;
+
+import java.util.Scanner;
+
+public class PalindromeNumber {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int num = sc.nextInt();
+		int orgNum = num;
+		int rev = 0;
+		while(num!=0) {
+			rev = rev*10 + num%10;
+			num = num/10;
+		}
+		if (orgNum == rev) {
+			System.out.println(orgNum + " is a Palindrome number");
+		}
+		else {
+			System.out.println(orgNum + " is not a Palindrome number");
+		}		
+	}
+}
