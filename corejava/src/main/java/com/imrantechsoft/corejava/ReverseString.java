ada560c35d5faef5cfd77c9df004bac4ca4b2995
Imran <imranbashamg@gmail.com>
2021-03-27 11:51:41 +0530

    Added StringReverse program to project

---
+++ corejava/src/main/java/com/imrantechsoft/corejava/ReverseString.java
@@ -0,0 +1,41 @@
+package com.imrantechsoft.corejava;
+
+import java.util.Scanner;
+
+public class ReverseString {
+	
+	public static void main(String[] args) {
+			
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a string: ");
+		String input = sc.next();
+		
+		
+		// Using concatenation operator '+'
+		/*
+		String rev = "";
+		int leng = input.length();
+		for(int i = leng-1; i>=0; i--)
+		{
+			rev = rev + input.charAt(i);
+		}     */
+		
+		
+		// Using charArray
+	/*  String rev = "";
+		char a[] =  input.toCharArray();
+		int leng = a.length;
+		for(int i = leng -1; i>=0; i--)
+		{
+			rev = rev + a[i];
+		}  */
+		
+		
+		// Using StringBuffer
+		StringBuffer sb = new StringBuffer(input);
+		StringBuffer rev = sb.reverse();
+		System.out.println("Reversed String is: " +rev);
+
+	}
+
+}
