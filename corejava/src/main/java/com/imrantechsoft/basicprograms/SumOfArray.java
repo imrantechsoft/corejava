c483ce5ed17c7927e8babe832e309856f5a3f5a2
Imran <imranbashamg@gmail.com>
2021-03-27 20:11:18 +0530

    Added SumOfArray program to the project

---
+++ corejava/src/main/java/com/imrantechsoft/basicprograms/SumOfArray.java
@@ -0,0 +1,15 @@
+package com.imrantechsoft.basicprograms;
+
+public class SumOfArray {
+
+	public static void main(String[] args) {
+
+		int a[] = {2,3,4,5,6,7,8,9};
+		int sum = 0;
+
+		for(int i=0; i<=a.length-1;i++) {
+			sum = sum+a[i];
+		}
+		System.out.println("Sum of Array Elements: "+sum);
+	}
+}
