1092a65dd49278ed180bbf712da082015ca1a192
Imran <imranbashamg@gmail.com>
2021-03-27 19:50:51 +0530

    Added Random Program and Apache commons dependency to POM

---
+++ corejava/src/main/java/com/imrantechsoft/corejava/GenerateRandomNumbersAndStrings.java
@@ -0,0 +1,27 @@
+package com.imrantechsoft.corejava;
+
+import java.util.Random;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class GenerateRandomNumbersAndStrings {
+
+	public static void main(String[] args) {
+
+		// Approach1 - Random
+		Random rand = new Random();
+		int randInt = rand.nextInt(100);
+		System.out.println("The random number is : "+randInt);
+
+		//Appraoch2 - Math
+		Double randDb = Math.random();
+		System.out.println("The random double number is :"+randDb);
+
+		//Approach3 - Apache commons-lang API
+		String randNum = RandomStringUtils.randomNumeric(10);
+		System.out.println("The random number is: "+randNum);
+
+		String randAlph = RandomStringUtils.randomAlphabetic(10);
+		System.out.println("The random number is: "+randAlph);
+	}
+}
