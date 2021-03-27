146d1dc19750a69a254c6e3690173775210aef5a
Imran <imranbashamg@gmail.com>
2021-03-27 19:23:31 +0530

    Added PrimeNumber program to the project

---
+++ corejava/src/main/java/com/imrantechsoft/corejava/PrimeNumber.java
@@ -0,0 +1,29 @@
+package com.imrantechsoft.corejava;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please Enter the Number: ");
+		int num=sc.nextInt();
+		int count=0; 
+
+		if(num>1) {
+			for(int i=1; i<=num; i++) {
+				if(num%i==0)
+					count++;
+			}
+			if(count==2) {
+				System.out.println(num+" is a Prime Number");
+			}else {
+				System.out.println(num+" is not a Prime Number");
+			}
+		}else {
+			System.out.println(num+" is not a Prime Number");
+		}
+		sc.close();
+	}
+}
